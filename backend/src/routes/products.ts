59b7de8c09360844bd5161e7bceeabb8e788bfa5
Bruno Felipe <brunofelipehp8@gmail.com>
2024-01-16 17:42:44 -0300

    finalized product routes

--- backend/src/routes/products.ts
+++ backend/src/routes/products.ts
@@ -3,76 +3,118 @@ import { prisma } from "../lib/prisma";
 import { z } from "zod";
 
 export async function productRoutes(app: FastifyInstance) {
-  app.get("/product", async () => {
-    const products = await prisma.product.findMany();
+  app.get("/product", async (request, reply) => {
+    try {
+      const products = await prisma.product.findMany();
 
-    return products;
+      return reply.code(200).send(products);
+    } catch (error) {
+      return reply.code(404).send("Error when searching for products!!!");
+    }
   });
 
-  app.post("/product", async (request) => {
-    const bodySchema = z.object({
-      name: z.string(),
-      description: z.string(),
-      color: z.string(),
-      price: z.number(),
-    });
+  app.get("/product/:id", async (request, reply) => {
+    try {
+      const paramsSchema = z.object({
+        id: z.string().uuid(),
+      });
 
-    const { name, description, color, price } = bodySchema.parse(request.body);
+      const { id } = paramsSchema.parse(request.params);
 
-    const product = await prisma.product.create({
-      data: {
-        name,
-        description,
-        color,
-        price,
-      },
-    });
+      const products = await prisma.product.findFirstOrThrow({
+        where: {
+          id,
+        },
+      });
 
-    return product;
+      return reply.code(200).send(products);
+    } catch (error) {
+      return reply.code(404).send("Error when searching for products!!!");
+    }
+  });
+
+  app.post("/product", async (request, reply) => {
+    try {
+      const bodySchema = z.object({
+        name: z.string(),
+        description: z.string(),
+        color: z.string(),
+        price: z.number(),
+      });
+
+      const { name, description, color, price } = bodySchema.parse(
+        request.body
+      );
+
+      const product = await prisma.product.create({
+        data: {
+          name,
+          description,
+          color,
+          price,
+        },
+      });
+
+      return reply.code(201).send(product);
+    } catch (error) {
+      return reply.code(400).send("error when registering a new product!!!");
+    }
   });
 
   app.put("/product/:id", async (request, reply) => {
-    const paramsSchema = z.object({
-      id: z.string().uuid(),
-    });
+    try {
+      const paramsSchema = z.object({
+        id: z.string().uuid(),
+      });
 
-    const { id } = paramsSchema.parse(request.params);
+      const { id } = paramsSchema.parse(request.params);
 
-    const bodySchema = z.object({
-      name: z.string(),
-      description: z.string(),
-      color: z.string(),
-      price: z.number(),
-    });
+      const bodySchema = z.object({
+        name: z.string(),
+        description: z.string(),
+        color: z.string(),
+        price: z.number(),
+      });
 
-    const { name, description, color, price } = bodySchema.parse(request.body);
+      const { name, description, color, price } = bodySchema.parse(
+        request.body
+      );
 
-    const product = await prisma.product.update({
-      where: {
-        id,
-      },
-      data: {
-        name,
-        description,
-        color,
-        price,
-      },
-    });
+      const product = await prisma.product.update({
+        where: {
+          id,
+        },
+        data: {
+          name,
+          description,
+          color,
+          price,
+        },
+      });
 
-    return product;
+      return reply.code(200).send(product);
+    } catch (error) {
+      return reply.code(400).send("Error. Unable to edit product");
+    }
   });
 
   app.delete("/product/:id", async (req, reply) => {
-    const paramsSchema = z.object({
-      id: z.string().uuid(),
-    });
+    try {
+      const paramsSchema = z.object({
+        id: z.string().uuid(),
+      });
 
-    const { id } = paramsSchema.parse(req.params);
+      const { id } = paramsSchema.parse(req.params);
 
-    await prisma.product.delete({
-      where: {
-        id,
-      },
-    });
+      await prisma.product.delete({
+        where: {
+          id,
+        },
+      });
+
+      return reply.code(200).send("Product deleted successfully");
+    } catch (error) {
+      return reply.code(404).send("Error. Unable to delete product");
+    }
   });
 }
