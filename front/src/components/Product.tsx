4a1c53a2f906ea96ff3f4011b0a25f352d5400d2
Bruno Felipe <brunofelipehp8@gmail.com>
2024-01-17 23:37:35 -0300

    creating the front-end

---
+++ front/src/components/Product.tsx
@@ -0,0 +1,19 @@
+export default function Product() {
+  function getProducts() {}
+  return (
+    <div className="grid justify-center content-center">
+      <div className="grid justify-center p-6 m-7 gap-5  border-2 border-blue-900 rounded-lg">
+        <h2>Notebook</h2>
+        <div className="flex justify-center gap-16">
+          <p className="w-96">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+            reprehenderit amet unde quas, voluptatibus nam dolorem quos dolores
+            sequi nulla assumenda eum? Deserunt quisquam quo dolorem et
+            molestias est natus.
+          </p>
+          <span>R$ 2500,00</span>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ front/src/components/Products.tsx
@@ -0,0 +1,9 @@
+import Product from "./Product";
+
+export default function Products() {
+  return (
+    <div>
+      <Product />
+    </div>
+  );
+}
